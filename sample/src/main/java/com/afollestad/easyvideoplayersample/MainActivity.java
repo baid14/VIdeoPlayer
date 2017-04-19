031b626b4151d12061f4956c20ff44c6a290156e
baid14 <baid@o2.pl>
2017-04-19 17:26:00 +0200

    first

---
+++ sample/src/main/java/com/afollestad/easyvideoplayersample/MainActivity.java
@@ -0,0 +1,87 @@
+package com.afollestad.easyvideoplayersample;
+import com.afollestad.easyvideoplayer.EasyVideoPlayer;
+import com.afollestad.materialdialogs.MaterialDialog;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.afollestad.easyvideoplayer.EasyVideoCallback;
+
+
+public class MainActivity extends AppCompatActivity implements EasyVideoCallback {
+
+    private EasyVideoPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        player = (EasyVideoPlayer) findViewById(R.id.player);
+        assert player != null;
+        player.setCallback(this);
+     
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        player.pause();
+    }
+    @Override
+    public void onPaused(EasyVideoPlayer player) {
+    }
+
+    @Override
+    public void onPreparing(EasyVideoPlayer player) {
+        Log.d("EVP-Sample", "onPreparing()");
+    }
+
+    @Override
+    public void onPrepared(EasyVideoPlayer player) {
+        Log.d("EVP-Sample", "onPrepared()");
+    }
+    @Override
+    public void onStarted(EasyVideoPlayer player) {
+    }
+
+
+
+    @Override
+    public void onBuffering(int percent) {
+        Log.d("EVP-Sample", "onBuffering(): " + percent + "%");
+    }
+
+    @Override
+    public void onError(EasyVideoPlayer player, Exception e) {
+        Log.d("EVP-Sample", "onError(): " + e.getMessage());
+        new MaterialDialog.Builder(this)
+                .title(R.string.error)
+                .content(e.getMessage())
+                .positiveText(android.R.string.ok)
+                .show();
+    }
+    @Override
+    public void onRetry(EasyVideoPlayer player, Uri source) {
+        Toast.makeText(this, "Retry", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSubmit(EasyVideoPlayer player, Uri source) {
+        Toast.makeText(this, "Submit", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClickVideoFrame(EasyVideoPlayer player) {
+        Toast.makeText(this, "Click video frame.", Toast.LENGTH_SHORT).show();
+
+    }
+    @Override
+    public void onCompletion(EasyVideoPlayer player) {
+        Log.d("EVP-Sample", "onCompletion()");
+    }
+
+
+}
